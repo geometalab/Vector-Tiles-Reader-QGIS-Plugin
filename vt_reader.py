e29c2a2c984135af530e4090633fc2a072b024c0
samwisee <mboos@outlook.com>
2017-03-03 14:18:30 +0100

    refactorings

--- vt_reader.py
+++ vt_reader.py
@@ -49,6 +49,14 @@ class VtReader:
         for f in files:
             os.remove(f)
 
+    def reset_json_template(self):
+        print "resetting template"
+        self.json_template = {
+            GeoTypes.POINT: {}, 
+            GeoTypes.LINE_STRING: {}, 
+            GeoTypes.POLYGON: {}}
+        print "template reset: ", self.json_template 
+
     def import_libs(self):        
         site.addsitedir(os.path.join(self.temp_dir, '/ext-libs'))
         print "import google.protobuf"
@@ -60,10 +68,10 @@ class VtReader:
 
     def do_work(self):
         self.clear_temp_dir()
-        self._create_layer()
         self.connect_to_db() 
-        row = self.get_data_from_db()
-        self.handle_single_record(row)
+        rows = self.get_data_from_db()
+        self.init_json_template()
+        self.handle_all_rows(rows)
 
     def connect_to_db(self):
         try:
@@ -76,18 +84,48 @@ class VtReader:
     def get_data_from_db(self):
         print "Reading data from db"
         zoom_level = 14
-        sql_command = "SELECT * FROM tiles WHERE zoom_level = {} LIMIT 1;".format(zoom_level)
+        sql_command = "SELECT * FROM tiles WHERE zoom_level = {} LIMIT 3;".format(zoom_level)
+        rows = []
         try:
             cur = self.conn.cursor()
-            cur.execute(sql_command)
-            row = cur.fetchone()
-            print "Record loaded:", row            
+            for row in cur.execute(sql_command):
+                #print "Record loaded:", row    
+                rows.append(row)                       
         except:
             print "Getting data from db failed:", sys.exc_info()
             return
-        return row
+        return rows
+
+    def init_json_template(self):
+        # create a layer for each type
+        print "create a layer for each type"
+        for value in self.geo_types:
+            self.json_template[self.geo_types[value]] = {
+                "type": "FeatureCollection", 
+                "crs": { # crs = coordinate reference system
+                    "type": "name", 
+                    "properties": {
+                        "name": "urn:ogc:def:crs:EPSG::3857"
+                        }
+                }, 
+                        "features": []
+            }
+        #print "Template: ", self.json_template
+
+    def handle_all_rows(self, rowSet):
+        base_template = self.json_template
+        for row in rowSet:
+            #self.init_json_template()
+            decoded_data, geometry = self.decode_row(row)            
+            self.write_features(decoded_data, geometry)
+            print "template size: ", len(self.json_template)
+
+        # the layers are only created once
+        # this means one vector layer per geotype will be added in qgis
+        # the other option would be to call this method once per row, then we have to reinitialize the json template for each row again
+        self.create_all_layers()        
  
-    def handle_single_record(self, row):
+    def decode_row(self, row):
         print "Handling record"
         try:
             geometry = [row[0], row[1], row[2]]
@@ -104,8 +142,9 @@ class VtReader:
             print "decoding data with mapbox_vector_tile failed", sys.exc_info()
             return
         #print decoded_data
+        return decoded_data, geometry
 
-        self.write_features(decoded_data, geometry)
+    def create_all_layers(self):
         for value in self.geo_types:
             file_src = self.create_unique_file_name()
             with open(file_src, "w") as f:
@@ -115,23 +154,7 @@ class VtReader:
     def add_vector_layer(self, json_src, layer_name):
         # load the created geojson into qgis
         layer = QgsVectorLayer(json_src, layer_name, "ogr")
-        QgsMapLayerRegistry.instance().addMapLayer(layer)
-
-    def _create_layer(self):
-        # create a layer for each type
-        print "create a layer for each type"
-        for value in self.geo_types:
-            self.json_template[self.geo_types[value]] = {
-                "type": "FeatureCollection", 
-                "crs": { # crs = coordinate reference system
-                    "type": "name", 
-                    "properties": {
-                        "name": "urn:ogc:def:crs:EPSG::3857"
-                        }
-                }, 
-                        "features": []
-            }
-        print "Template: ", self.json_template
+        QgsMapLayerRegistry.instance().addMapLayer(layer)    
 
     def write_features(self, decoded_data, geometry):
         print "Now creating proper GeoJSON"
@@ -148,37 +171,39 @@ class VtReader:
                 #break
         #print self.json_template
 
-    def create_feature_json(self, data, geometry):
-        data_type = data["type"]
+    def create_feature_json(self, feature, geometry):
+        #print "feature: ", feature
+        feature_type = feature["type"]
         #  single feature structure
-        geo_type = self.geo_types[data_type]
-        coordinates = self._mercator_geometry(geo_type, data["geometry"], geometry, 0)        
+        geo_type = self.geo_types[feature_type]
+        coordinates = self._mercator_geometry(geo_type, feature["geometry"], geometry, 0)
 
         if geo_type == GeoTypes.POINT:
-            assert data_type == 1
+            assert feature_type == 1
             # Due to mercator_geometrys nature, the point will be displayed in a List "[[]]", remove the outer bracket.
             coordinates = coordinates[0]
         if geo_type == GeoTypes.POLYGON and self._counter == 0:
-            assert data_type == 3
+            assert feature_type == 3
             # If there is not a polygon in a polygon, one bracket will be missing.
-            coordinates = [coordinates]
+            coordinates = [coordinates]        
 
-        feature = None
+        feature_json = None
+
         # if there is no MultiLineString create a new feature
-        # if there IS a MultiLineString the counter will be greater than zero
-        if data_type != 2 or self._counter == 0:
-            feature = {
+        # if there IS a MultiLineString the counter will be greater than zero and NO feature will be created
+        if feature_type != 2 or self._counter == 0:
+            feature_json = {
                 "type": "Feature",
                 "geometry": {
                     "type": geo_type,
                     "coordinates": coordinates
                 },
-                "properties": data["properties"]
+                "properties": feature["properties"]
             }
 
         self._counter = 0
         self._bool = True
-        return feature, geo_type
+        return feature_json, geo_type
 
     def _mercator_geometry(self, geo_type, coordinates, geometry, counter):
         # print "mercator iteration {}".format(counter)
@@ -192,12 +217,14 @@ class VtReader:
         for index, coord in enumerate(coordinates):
             is_multi = not isinstance(coord[0], int)
             multi_string = "Multi" if is_multi else ""
-            # print "---- coord: {} (Type: {})".format(coord, "{}{}".format(multi_string, geo_type))
+            # if is_multi:
+            #     print "---- coord: {} (Type: {})".format(coord, "{}{}".format(multi_string, geo_type))
 
             if not is_multi:
                 tmp.append(self._calculate_geometry(self, coord, geometry))
             else:
                 tmp.append(self._mercator_geometry(geo_type, coord, geometry, counter + 1))
+
         if self._bool:
             self._counter = counter
             self._bool = False
