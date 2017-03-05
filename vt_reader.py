5aa4c16992053aa51210aaca7d6d4678d51de94b
samwisee <mboos@outlook.com>
2017-03-05 09:21:19 +0100

    build a group hierarchy in qgis the hierarchy depends on the class and subclass of the feature

--- vt_reader.py
+++ vt_reader.py
@@ -28,8 +28,7 @@ class VtReader:
     _extent = 4096    
     directory = os.path.abspath(os.path.dirname(__file__))
     temp_dir = "%s/tmp" % directory
-    file_path = "%s/sample data/zurich_switzerland.mbtiles" % directory
-    all_features = {}
+    file_path = "%s/sample data/zurich_switzerland.mbtiles" % directory    
     
     def __init__(self, iface):
         self.iface = iface
@@ -38,7 +37,22 @@ class VtReader:
         self._bool = True
         self._mbtile_id = "name"
 
+    def reinit(self):
+        """
+         * Reinitializes the VtReader
+         >> Cleans the temp directory
+         >> Cleans the feature cache
+         >> Cleans the qgis group cache
+        """
+
+        self.clear_temp_dir()
+        self.features_by_path = {}
+        self.qgis_layer_groups_by_feature_path = {}
+
     def clear_temp_dir(self):
+        """
+         * Removes all files from the temp_dir
+        """
         if not os.path.exists(self.temp_dir):
             os.makedirs(self.temp_dir)
         files = glob.glob("%s/*" % self.temp_dir)
@@ -46,6 +60,10 @@ class VtReader:
             os.remove(f)
 
     def get_empty_feature_collection(self):
+        """
+         * Returns an empty GeoJSON FeatureCollection
+        """
+
         from geojson import FeatureCollection
         crs = { # crs = coordinate reference system
                 "type": "name", 
@@ -53,7 +71,11 @@ class VtReader:
                     "name": "urn:ogc:def:crs:EPSG::3857" } }        
         return FeatureCollection([], crs=crs)
 
-    def import_libs(self):        
+    def import_libs(self):
+        """
+         * Imports the external libraries that are required by this plugin
+        """
+
         site.addsitedir(os.path.join(self.temp_dir, '/ext-libs'))
         self.import_library("google.protobuf")
         self.mvt = self.import_library("mapbox_vector_tile")
@@ -66,13 +88,17 @@ class VtReader:
         return module
 
     def do_work(self):
-        self.clear_temp_dir()
+        self.reinit()
         self.connect_to_db() 
         tile_data_tuples = self.load_tiles_from_db()
         tiles = self.decode_all_tiles(tile_data_tuples)
         self.process_tiles(tiles)
 
     def connect_to_db(self):
+        """
+         * Since an mbtile file is a sqlite database, we can connect to it
+        """
+
         try:
             self.conn = sqlite3.connect(self.file_path)
             self.conn.row_factory = sqlite3.Row
@@ -114,7 +140,7 @@ class VtReader:
         for index, tile in enumerate(tiles):          
             self.write_features(tile)
             print "Progress: {0:.1f}%".format(100.0 / totalNrTiles * (index+1))
-        self.create_layers_for_geotypes()        
+        self.create_qgis_layer_hierarchy()        
 
     def decode_binary_tile_data(self, data):
         try:
@@ -125,19 +151,56 @@ class VtReader:
             print "decoding data with mapbox_vector_tile failed", sys.exc_info()
             return
         return decoded_data
+    
+    def create_qgis_layer_hierarchy(self):
+        """
+         * Creates a hierarchy of groups and layers in qgis
+        """
 
-    def create_layers_for_geotypes(self):
         root = QgsProject.instance().layerTreeRoot()
         group_name = os.path.splitext(os.path.basename(self.file_path))[0]
         rootGroup = root.addGroup(group_name)
-        for feature_path in self.all_features:
-            feature_collection = self.all_features[feature_path]                      
+        for feature_path in self.features_by_path:
+            target_group, layer_name = self.get_group_for_path(feature_path, rootGroup)
+            feature_collection = self.features_by_path[feature_path]                      
             file_src = self.create_unique_file_name()
             with open(file_src, "w") as f:
                 json.dump(feature_collection, f)
-            self.add_vector_layer(file_src, feature_path, rootGroup)
+            self.add_vector_layer(file_src, layer_name, target_group)
+
+    def get_group_for_path(self, path, root_group):
+        """
+         * Returns the group for the specified path
+         >> If the group not already exists, it will be created
+         >> The path has to be delimited by '.'
+         >> The last element in the path will be used as name for the vector layer, the other elements will be used to create the group hierarchy
+         >> Example: The path 'zurich.poi.police' will create two groups 'zurich' and 'poi' (if not already existing) and 'police' will be returned as name for the layer to create
+        """
+
+        group_names = path.split(".")
+        current_group = root_group
+        current_path = ""
+        target_layer_name = ""
+        for index, name in enumerate(group_names):
+            target_layer_name = name
+            is_last = index == len(group_names)-1
+            if is_last:
+                break
+            current_path += "."+name
+            if not current_path in self.qgis_layer_groups_by_feature_path:
+                self.qgis_layer_groups_by_feature_path[current_path] = self.create_group(name, current_group)
+            current_group = self.qgis_layer_groups_by_feature_path[current_path]
+        return current_group, target_layer_name
+
+    def create_group(self, name, parent_group):
+        new_group = parent_group.addGroup(name)
+        return new_group        
 
     def add_vector_layer(self, json_src, layer_name, layer_target_group):
+        """
+         * Creates a QgsVectorLayer and adds it to the group specified by layer_target_group
+        """
+
         # load the created geojson into qgis
         layer = QgsVectorLayer(json_src, layer_name, "ogr")
         QgsMapLayerRegistry.instance().addMapLayer(layer, False)    
@@ -150,33 +213,40 @@ class VtReader:
             print "Handle features of layer: ", layer_name
             tile_features = tile.decoded_data[layer_name]["features"]
             for index, feature in enumerate(tile_features):
-                data, geo_type = self.create_geojson_feature(feature, tile)
-                if data:
-                    feature_class = None
-                    feature_subclass = None
-                    if "class" in data.properties:
-                        feature_class = data.properties["class"]
-                        if "subclass" in data.properties:
-                            feature_subclass = data.properties["subclass"]
-                    
-                    if feature_subclass:
-                        assert feature_class, "A feature with a subclass should also have a class"
+                geojson_feature, geo_type = self.create_geojson_feature(feature, tile)
+                if geojson_feature:
+                    feature_path = self.get_feature_path(layer_name, geojson_feature)
+                    if not feature_path in self.features_by_path:
+                        self.features_by_path[feature_path] = self.get_empty_feature_collection()
 
-                    feature_path = layer_name;
-                    if feature_class:
-                        feature_path += "." + feature_class
-                        if feature_subclass:
-                            feature_path += "." +feature_subclass
-
-                    if not feature_path in self.all_features:
-                        self.all_features[feature_path] = self.get_empty_feature_collection()
-
-                    self.all_features[feature_path].features.append(data)
+                    self.features_by_path[feature_path].features.append(geojson_feature)
                 
                 # TODO: remove the break after debugging
                 # print "feature: ", feature
                 # print "   data: ", data
                 #break
+
+    def get_feature_class_and_subclass(self, feature):
+        feature_class = None
+        feature_subclass = None
+        if "class" in feature.properties:
+            feature_class = feature.properties["class"]
+            if "subclass" in feature.properties:
+                feature_subclass = feature.properties["subclass"]
+                if feature_subclass == feature_class:
+                    feature_subclass = None
+        if feature_subclass:
+            assert feature_class, "A feature with a subclass should also have a class"
+        return feature_class, feature_subclass
+
+    def get_feature_path(self, layer_name, feature):
+        feature_class, feature_subclass = self.get_feature_class_and_subclass(feature) 
+        feature_path = layer_name;
+        if feature_class:
+            feature_path += "." + feature_class
+            if feature_subclass:
+                feature_path += "." +feature_subclass
+        return feature_path        
 
     def create_geojson_feature(self, feature, tile):
         """
