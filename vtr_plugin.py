2e739b7fdc76f6fd55ed07b1fa92aaff88ac9833
samwisee <mboos@outlook.com>
2017-03-17 17:22:32 +0100

    plugin button extended: filebrowser added

--- vtr_plugin.py
+++ vtr_plugin.py
@@ -13,8 +13,8 @@ of the License, or (at your option) any later version.
 
 """
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4.QtCore import QSettings
+from PyQt4.QtGui import QAction, QIcon, QMenu, QToolButton, QFileDialog
 from qgis.core import *
 from log_helper import info
 
@@ -31,20 +31,51 @@ class VtrPlugin:
 
     def __init__(self, iface):
         self.iface = iface
-        self.settings = QSettings("Vector Tile Reader","vectortilereader")
+        self.settings = QSettings("Vector Tile Reader", "vectortilereader")
+        self._init_openfile_dialog()
+
+    def _init_openfile_dialog(self):
+        dlg = QFileDialog()
+        dlg.setNameFilter("Mapbox Tiles (*.mbtiles)")
+        dlg.setOption(QFileDialog.DontUseNativeDialog)
+        self.open_mbtile_dialog = dlg
+
 
     def initGui(self):
         self.action = QAction(QIcon(':/plugins/vectortilereader/icon.png'), "Add Vector Tiles Layer", self.iface.mainWindow())
         self.action.triggered.connect(self.run)
-        self.iface.addVectorToolBarIcon(self.action)
+        # self.iface.addVectorToolBarIcon(self.action)
         self.iface.addPluginToMenu("&Vector Tiles Reader", self.action)
         self.iface.addPluginToVectorMenu("&Vector Tiles Reader", self.action)
-
         self.settingsaction = QAction(QIcon(':/plugins/vectortilereader/icon.png'), "Settings", self.iface.mainWindow())
         self.settingsaction.triggered.connect(self.edit_sources)
         self.iface.addPluginToMenu("&Vector Tiles Reader", self.settingsaction)
         self.init_vt_reader()
+        self.add_menu()
         info("Vector Tile Reader Plugin loaded...")
+
+    def add_menu(self):
+        self.popupMenu = QMenu(self.iface.mainWindow())
+        default_action = self._create_action("Add Vector Tiles Layer", "icon.png", self.run)
+        self.popupMenu.addAction(default_action)
+        self.popupMenu.addAction(self._create_action("Choose Mapbox Tiles File (...)", "folder.svg", self._open_file_browser))
+
+        self.toolButton = QToolButton()
+        self.toolButton.setMenu(self.popupMenu)
+        self.toolButton.setDefaultAction(default_action)
+        self.toolButton.setPopupMode(QToolButton.MenuButtonPopup)
+        self.toolButtonAction = self.iface.addVectorToolBarWidget(self.toolButton)
+
+    def _open_file_browser(self):
+        path = QFileDialog.getOpenFileName(self.iface.mainWindow(), "Select Mapbox Tiles", "", "Mapbox Tiles (*.mbtiles)")
+        # path = self.open_mbtile_dialog.exec_()
+        if path:
+            print("Selected: {}".format(path))
+
+    def _create_action(self, title, icon, callback):
+        new_action = QAction(QIcon(':/plugins/vectortilereader/{}'.format(icon)), title, self.iface.mainWindow())
+        new_action.triggered.connect(callback)
+        return new_action
 
     def init_vt_reader(self):
         self._add_path_to_dependencies_to_syspath()
@@ -62,7 +93,8 @@ class VtrPlugin:
 
     def unload(self):
         # Remove the plugin menu item and icon
-        self.iface.removeVectorToolBarIcon(self.action)
+        # self.iface.removeVectorToolBarIcon(self.action)
+        self.iface.removeVectorToolBarIcon(self.toolButtonAction)
         self.iface.removePluginMenu("&Vector Tiles Reader", self.action)
         self.iface.removePluginVectorMenu("&Vector Tiles Reader", self.action)
         self.iface.removePluginMenu("&Vector Tiles Reader", self.settingsaction)
