4a56949b4d8ebe588485f71022557fefea94e118
Martin Boos <mboos@outlook.com>
2017-04-10 14:32:00 +0200

    exception handling in log_helper

--- log_helper.py
+++ log_helper.py
@@ -1,5 +1,6 @@
 from qgis.core import QgsMessageLog
 import logging
+import sys
 
 
 def info(msg, *args):
@@ -19,18 +20,21 @@ def debug(msg, *args):
 
 
 def _log_message(msg, level, *args):
-    msg = msg.format(*args)
+    try:
+        msg = msg.format(*args)
 
-    if level == QgsMessageLog.INFO:
-        logging.info(msg)
-    elif level == QgsMessageLog.WARNING:
-        logging.warning(msg)
-    elif level == QgsMessageLog.CRITICAL:
-        logging.critical(msg)
-    elif not level:
-        logging.debug(msg)
-        # TODO: disable after debugging
-        level = QgsMessageLog.INFO
+        if level == QgsMessageLog.INFO:
+            logging.info(msg)
+        elif level == QgsMessageLog.WARNING:
+            logging.warning(msg)
+        elif level == QgsMessageLog.CRITICAL:
+            logging.critical(msg)
+        elif not level:
+            logging.debug(msg)
+            # TODO: disable after debugging
+            level = QgsMessageLog.INFO
 
-    print(msg)
-    QgsMessageLog.logMessage(msg, 'Vector Tile Reader', level)
+        print(msg)
+        QgsMessageLog.logMessage(msg, 'Vector Tile Reader', level)
+    except:
+        print("Something went wrong: {}".format(sys.exc_info()))
