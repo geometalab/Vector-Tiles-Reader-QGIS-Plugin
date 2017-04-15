f1e5c03a6dd4210ccb98210cc874a7833e1d16c6
samwisee <mboos@outlook.com>
2017-04-15 11:44:39 +0200

    * use scheme from TileJSON to calculate tile x/y

--- tile_helper.py
+++ tile_helper.py
@@ -22,13 +22,16 @@ def coordinate_to_tile(zoom, lat, lng):
     return tile
 
 
-def get_tile_bounds(zoom, bounds):
+def get_tile_bounds(zoom, bounds, scheme="xyz"):
     """
-     * Returns the tile boundaries in the form [(x_min, y_min), (x_max, y_max)] where both values are tuples
+     * Returns the tile boundaries in XYZ scheme in the form [(x_min, y_min), (x_max, y_max)] where both values are tuples
+    :param scheme: 
     :param zoom: 
     :param bounds: 
     :return: 
     """
+    if scheme not in ["xyz", "tms"]:
+        raise RuntimeError("Scheme not supported: {}".format(scheme))
 
     tiles = None
     if bounds:
@@ -39,5 +42,15 @@ def get_tile_bounds(zoom, bounds):
 
         xy_min = coordinate_to_tile(zoom, lat_max, lng_min)
         xy_max = coordinate_to_tile(zoom, lat_min, lng_max)
+
+        is_tms_scheme = scheme == "tms"
+        if is_tms_scheme:
+            xy_min = (xy_min[0], _change_scheme(zoom, xy_min[1]))
+            xy_max = (xy_max[0], _change_scheme(zoom, xy_max[1]))
+
         tiles = [xy_min, xy_max]
     return tiles
+
+
+def _change_scheme(zoom, y):
+    return (2 ** zoom) - y - 1
