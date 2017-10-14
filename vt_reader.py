32954a8ac5c48f657accee3a7e15d8234889dfd8
samwisee <mboos@outlook.com>
2017-10-14 11:48:12 +0200

    cleanup when a project is created/loaded

--- vt_reader.py
+++ vt_reader.py
@@ -119,6 +119,7 @@ class VtReader(QObject):
         return source
 
     def shutdown(self):
+        info("Shutdown reader")
         self.source.progress_changed.disconnect()
         self.source.max_progress_changed.disconnect()
         self.source.message_changed.disconnect()
