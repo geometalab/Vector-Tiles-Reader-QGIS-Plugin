a3d94937d79ae34ede6a576a39865590181371b1
samwisee <mboos@outlook.com>
2017-10-23 14:06:02 +0200

    use pytest

--- test_vtreader.py
+++ test_vtreader.py
@@ -2,7 +2,7 @@
 #
 # This code is licensed under the GPL 2.0 license.
 #
-import unittest
+import pytest
 import os
 import sys
 from qgis.core import *
@@ -12,54 +12,17 @@ from PyQt4.QtCore import *
 from tile_source import MBTilesSource
 
 
-class IfaceTests(unittest.TestCase):
-    """
-    Tests for Iface
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
-
-    def testIfaceisNotNote(self):
-        global iface
-        self.assertIsNotNone(iface)
+def testIfaceisNotNote():
+    global iface
+    assert iface is not None
 
 
-class MbtilesSourceTests(unittest.TestCase):
-    """
-    Tests for MBTilesSource
-    """
-
-    @classmethod
-    def setUpClass(cls):
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
-
-    def testMbtilesSourceCreation(self):
-        path = os.path.join(os.path.dirname(__file__), 'sample_data', 'uster_zh.mbtiles')
-        src = MBTilesSource(path)
-        self.assertEqual(path, src.source())
-
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(IfaceTests))
-    # suite = unittest.makeSuite(IfaceTests, 'test')
-    return suite
-
-
-# run all tests using unittest skipping nose or testplugin
-def run_all():
-    unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(suite())
+def testMbtilesSourceCreation(self):
+    path = os.path.join(os.path.dirname(__file__), 'sample_data', 'uster_zh.mbtiles')
+    src = MBTilesSource(path)
+    assert src is not None
+    assert path == src.source()
 
 
 if __name__ == "__main__":
-    run_all()
+    pytest.main()
