4df8170c918441667e0dcc97c6544bb3e510afd7
samwisee <mboos@outlook.com>
2017-10-23 14:20:01 +0200

    old file removed

--- test_vtreader.py
+++ test_vtreader.py
@@ -9,19 +9,17 @@ from qgis.core import *
 from qgis.utils import iface
 from PyQt4.QtCore import *
 
-from tile_source import MBTilesSource
-
 
 def testIfaceisNotNote():
     global iface
     assert iface is not None
 
 
-def testMbtilesSourceCreation():
-    path = os.path.join(os.path.dirname(__file__), 'sample_data', 'uster_zh.mbtiles')
-    src = MBTilesSource(path)
-    assert src is not None
-    assert path == src.source()
+# def testMbtilesSourceCreation():
+#     path = os.path.join(os.path.dirname(__file__), 'sample_data', 'uster_zh.mbtiles')
+#     src = MBTilesSource(path)
+#     assert src is not None
+#     assert path == src.source()
 
 
 if __name__ == "__main__":
