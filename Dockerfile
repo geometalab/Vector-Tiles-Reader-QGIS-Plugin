fc7a26398d8a53607918616d1653508c5c78eac7
mnboos <mboos@outlook.com>
2017-11-11 12:48:39 +0100

    report coverage

--- Dockerfile
+++ Dockerfile
@@ -14,3 +14,4 @@ ENV DISPLAY=:99
 RUN qgis_setup.sh vector_tiles_reader
 RUN rm -rf $QGIS_PLUGINS_DIR/vector_tiles_reader
 RUN ln -s /vector-tiles-reader $QGIS_PLUGINS_DIR/vector_tiles_reader
+RUN apt-get install python-pip && pip install coverage
--- tests/test_all.py
+++ tests/test_all.py
@@ -3,6 +3,7 @@
 # This code is licensed under the GPL 2.0 license.
 #
 import unittest
+import coverage
 import os
 import sys
 from qgis.core import *
@@ -26,11 +27,17 @@ def suites():
 
 # run all tests using unittest skipping nose or testplugin
 def run_all():
+    cov = coverage.Coverage()
+    cov.start()
     for s in suites():
         print("----------------------------------------------------------------------")
         print("")
         unittest.TextTestRunner(verbosity=3, stream=sys.stdout).run(s)
         print("")
+    cov.stop()
+    cov.save()
+    cov.html_report(directory='tests/htmlcov')
+    print cov.report()
 
 
 if __name__ == "__main__":
