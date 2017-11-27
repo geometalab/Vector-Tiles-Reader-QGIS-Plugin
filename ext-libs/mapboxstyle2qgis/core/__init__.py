6b1dcee997ace10b43c4587ee863d3acf936a6bb
samwisee <mboos@outlook.com>
2017-11-27 15:23:33 +0100

    style generator updated

--- ext-libs/mapboxstyle2qgis/core/__init__.py
+++ ext-libs/mapboxstyle2qgis/core/__init__.py
@@ -4,6 +4,7 @@ import copy
 import colorsys
 import urllib
 import base64
+import shutil
 from itertools import groupby
 from xml.sax.saxutils import escape
 from .xml_helper import create_style_file
@@ -117,6 +118,8 @@ def _create_icons(image_base64, image_definition_data, output_directory):
     icons_directory = os.path.join(output_directory, "icons")
     if not os.path.isdir(icons_directory):
         os.makedirs(icons_directory)
+    src_icon_path = os.path.join(os.path.dirname(__file__), "data", "icons", "empty.svg")
+    shutil.copy2(src_icon_path, icons_directory)
 
     template_path = os.path.join(os.path.dirname(__file__), "data", "svg_template.svg")
     assert os.path.isfile(template_path)
@@ -270,6 +273,7 @@ def get_styles(layer):
         all_values.extend(get_properties_by_zoom(layer, "paint/fill-outline-color", is_color=True))
         all_values.extend(get_properties_by_zoom(layer, "paint/fill-translate"))
         all_values.extend(get_properties_by_zoom(layer, "paint/fill-opacity"))
+        all_values.extend(get_properties_by_zoom(layer, "paint/fill-pattern", is_expression=True))
     elif layer_type == "line":
         all_values.extend(get_properties_by_zoom(layer, "layout/line-join"))
         all_values.extend(get_properties_by_zoom(layer, "layout/line-cap"))
@@ -278,9 +282,10 @@ def get_styles(layer):
         all_values.extend(get_properties_by_zoom(layer, "paint/line-opacity"))
         all_values.extend(get_properties_by_zoom(layer, "paint/line-dasharray"))
     elif layer_type == "symbol":
+        all_values.extend(get_properties_by_zoom(layer, "layout/icon-image", is_expression=True))
         all_values.extend(get_properties_by_zoom(layer, "layout/text-font"))
         all_values.extend(get_properties_by_zoom(layer, "layout/text-size", can_interpolate=True))
-        all_values.extend(get_properties_by_zoom(layer, "layout/text-field", is_expression=True))
+        all_values.extend(get_properties_by_zoom(layer, "layout/text-field", is_expression=True, take=1))
         all_values.extend(get_properties_by_zoom(layer, "layout/text-max-width"))
         all_values.extend(get_properties_by_zoom(layer, "paint/text-color", is_color=True))
         all_values.extend(get_properties_by_zoom(layer, "paint/text-halo-width", can_interpolate=True))
@@ -327,16 +332,55 @@ def get_styles(layer):
     return resulting_styles
 
 
-def _parse_expr(expr):
-    if not expr:
-        return expr
-    fields = expr.replace("{", '"').replace("}", '"').replace("\n", " ").strip().split(" ")
-    if fields:
-        return escape_xml(fields[0])
-        expr = _get_field_expr(0, fields)
-    # fields = map(lambda (index, f): _get_field_expr(index, f), enumerate(fields))
-    expr = "+".join(fields)
-    return escape_xml(expr)
+def _parse_expr(expr, take=None):
+    """
+     * Creates a QGIS expression
+    :param expr:
+    :param take: The nr of fields to take. All if value is None.
+                 E.g.: "{name:latin}\n{name:nonlatin}" consists of two fields
+    :return:
+    """
+
+    fields = _get_qgis_fields(expr)[:take]
+    result = "+".join(fields)
+    return escape_xml(result)
+
+
+def _map_value_to_qgis_expr(val):
+    if val["is_expr"]:
+        return '"{}"'.format(val["text"])
+    else:
+        return "'{}'".format(val["text"])
+
+
+def _get_qgis_fields(expr):
+    values = []
+    val = None
+    is_expr = False
+    for s in expr:
+        if not is_expr:
+            new_is_expr = s == '{'
+        elif s == '}':
+            new_is_expr = False
+        has_changed = new_is_expr != is_expr
+        is_expr = new_is_expr
+        if has_changed:
+            if val:
+                values.append(val)
+            val = None
+        if has_changed:
+            continue
+        if val is None:
+            val = {
+                "text": "",
+                "is_expr": is_expr
+            }
+
+        val["text"] += s
+    if val:
+        values.append(val)
+    mapped = list(map(_map_value_to_qgis_expr, values))
+    return mapped
 
 
 def escape_xml(value):
@@ -401,7 +445,7 @@ def _apply_scale_range(styles):
         s["max_scale_denom"] = max_scale_denom
 
 
-def get_properties_by_zoom(paint, property_path, is_color=False, is_expression=False, can_interpolate=False, default=None):
+def get_properties_by_zoom(paint, property_path, is_color=False, is_expression=False, can_interpolate=False, default=None, take=None):
     if (is_color or is_expression) and can_interpolate:
         raise RuntimeError("Colors and expressions cannot be interpolated")
 
@@ -431,7 +475,7 @@ def get_properties_by_zoom(paint, property_path, is_color=False, is_expression=F
             if is_color:
                 value = parse_color(value)
             if is_expression:
-                value = _parse_expr(value)
+                value = _parse_expr(value, take=take)
             if can_interpolate:
                 is_qgis_expr = True
                 next_stop = stops[index + 1]
@@ -456,7 +500,7 @@ def get_properties_by_zoom(paint, property_path, is_color=False, is_expression=F
         if is_color:
             value = parse_color(value)
         if is_expression:
-            value = _parse_expr(value)
+            value = _parse_expr(value, take=take)
         properties.append({
             "name": parts[-1],
             "zoom_level": None,
--- ext-libs/mapboxstyle2qgis/core/xml_helper.py
+++ ext-libs/mapboxstyle2qgis/core/xml_helper.py
@@ -29,16 +29,25 @@ def create_style_file(output_directory, layer_style):
     if "layer-transparency" in layer_style:
         layer_transparency = layer_style["layer-transparency"]
 
+    icons_directory = os.path.join(output_directory, "icons")
     for index, s in enumerate(layer_style["styles"]):
         if layer_type == "line":
             rules.append(_get_rule(index, s, rule_content=""))
             symbols.append(_get_line_symbol(index, s))
         elif layer_type == "fill":
             rules.append(_get_rule(index, s, rule_content=""))
-            symbols.append(_get_fill_symbol(index, s))
+            symbols.append(_get_fill_symbol(index, s, icons_directory=icons_directory))
         elif layer_type == "symbol":
             labeling_settings = _get_labeling_settings(s)
             labeling_rules.append(_get_rule(index, s, rule_content=labeling_settings))
+            if "icon-image" in s:
+                rules.append(_get_rule(index, s, rule_content=""))
+                icn = _get_icon_symbol(index=index,
+                                       style=s,
+                                       icons_directory=icons_directory,
+                                       icon_expr=s["icon-image"])
+                symbols.append(icn)
+
 
     rule_string = """<rules key="{key}">
     {rules}
@@ -131,54 +140,21 @@ def _get_labeling_settings(style):
                draw_buffer=draw_buffer)
 
 
-def _get_fill_symbol(index, style):
+def _get_icon_symbol(index, style, icons_directory, icon_expr):
     opacity = _get_value_safe(style, "fill-opacity", 1)
-    offset = list(map(lambda o: str(o), _get_value_safe(style, "fill-translate", default=[0, 0])))
-    offset = ",".join(offset)
-    fill_color_rgba = _get_value_safe(style, "fill-color", "")
-    fill_outline_color_rgba = _get_value_safe(style, "fill-outline-color", fill_color_rgba)
-    label = style["name"]
-    if style["zoom_level"] is not None:
-        label = "{}-zoom-{}".format(label, style["zoom_level"])
-
-    symbol = """<!-- {description} -->
-    <symbol alpha="{opacity}" clip_to_extent="1" type="fill" name="{index}">
-            <layer pass="{rendering_pass}" class="SimpleFill" locked="0">
-                <prop k="border_width_map_unit_scale" v="0,0,0,0,0,0"/>
-                <prop k="color" v="{fill_color}"/>
-                <prop k="joinstyle" v="bevel"/>
-                <prop k="offset" v="{offset}"/>
-                <prop k="offset_map_unit_scale" v="0,0,0,0,0,0"/>
-                <prop k="offset_unit" v="Pixel"/>
-                <prop k="outline_color" v="{fill_outline_color}"/>
-                <prop k="outline_style" v="solid"/>
-                <prop k="outline_width" v="0.7"/>
-                <prop k="outline_width_unit" v="Pixel"/>
-                <prop k="style" v="solid"/>
-            </layer>
-        </symbol>
-        """.format(opacity=opacity,
-                   index=index,
-                   fill_color=fill_color_rgba,
-                   fill_outline_color=fill_outline_color_rgba,
-                   offset=offset,
-                   description=label,
-                   rendering_pass=style["rendering_pass"])
-    return symbol
-
-
-def _get_svg_symbol():
-    return """
-      <renderer-v2 forceraster="0" symbollevels="0" type="singleSymbol" enableorderby="0">
-    <symbols>
-      <symbol alpha="1" clip_to_extent="1" type="marker" name="0">
-        <layer pass="0" class="SvgMarker" locked="0">
+    svg_path = "'" + os.path.join(icons_directory, "'+{}+'.svg'".format(icon_expr)).replace("\\", "/")
+    fallback_path = "'" + os.path.join(icons_directory, "empty.svg'").replace("\\", "/")
+    svg_expr = "if_not_exists({svg_path}, {fallback_path})".format(svg_path=svg_path, fallback_path=fallback_path)
+    rendering_pass = _get_value_safe(style, "rendering_pass", 0)
+    return """<!-- {description} -->
+          <symbol alpha="{opacity}" clip_to_extent="1" type="marker" name="{index}">
+        <layer pass="{rendering_pass}" class="SvgMarker" locked="0">
           <prop k="angle" v="0"/>
           <prop k="color" v="132,172,217,255"/>
           <prop k="horizontal_anchor_point" v="1"/>
-          <prop k="name" v="./Downloads/method-draw-image.svg"/>
+          <prop k="name" v=""/>
           <prop k="name_dd_active" v="1"/>
-          <prop k="name_dd_expression" v="if_not_exists('C:/Users/Martin/Downloads/icons/'+ "class" + '-11.svg', 'C:/Users/Martin/Downloads/icons/empty.svg')"/>
+          <prop k="name_dd_expression" v="{svg_path}"/>
           <prop k="name_dd_field" v=""/>
           <prop k="name_dd_useexpr" v="1"/>
           <prop k="offset" v="0,0"/>
@@ -189,17 +165,124 @@ def _get_svg_symbol():
           <prop k="outline_width_map_unit_scale" v="0,0,0,0,0,0"/>
           <prop k="outline_width_unit" v="MM"/>
           <prop k="scale_method" v="diameter"/>
-          <prop k="size" v="11"/>
+          <prop k="size" v="17"/>
           <prop k="size_map_unit_scale" v="0,0,0,0,0,0"/>
           <prop k="size_unit" v="Pixel"/>
           <prop k="vertical_anchor_point" v="1"/>
         </layer>
       </symbol>
-    </symbols>
-    <rotation/>
-    <sizescale scalemethod="diameter"/>
-  </renderer-v2>
-    """
+    """.format(description=style["name"],
+               opacity=opacity,
+               svg_path=svg_expr,
+               index=index,
+               rendering_pass=rendering_pass)
+
+
+def _get_fill_symbol(index, style, icons_directory):
+    opacity = _get_value_safe(style, "fill-opacity", 1)
+    offset = list(map(lambda o: str(o), _get_value_safe(style, "fill-translate", default=[0, 0])))
+    offset = ",".join(offset)
+    fill_color_rgba = _get_value_safe(style, "fill-color", "")
+    fill_outline_color_rgba = _get_value_safe(style, "fill-outline-color", fill_color_rgba)
+    label = style["name"]
+    fill_pattern = _get_value_safe(style, "fill-pattern")
+    rendering_pass = _get_value_safe(style, "rendering_pass", 0)
+    if style["zoom_level"] is not None:
+        label = "{}-zoom-{}".format(label, style["zoom_level"])
+
+    if fill_pattern:
+        symbol = _get_fill_pattern_symbol_xml(pattern=fill_pattern,
+                                              label=label,
+                                              index=index,
+                                              opacity=opacity,
+                                              rendering_pass=rendering_pass,
+                                              icons_directory=icons_directory)
+    else:
+        symbol = _get_fill_symbol_xml(fill_color_rgba=fill_color_rgba,
+                                      fill_outline_color_rgba=fill_outline_color_rgba,
+                                      index=index,
+                                      label=label,
+                                      offset=offset,
+                                      opacity=opacity,
+                                      rendering_pass=rendering_pass)
+    return symbol
+
+
+def _get_fill_pattern_symbol_xml(pattern, label, index, opacity, rendering_pass, icons_directory):
+    svg_path = "'" + os.path.join(icons_directory, "'+{}+'.svg'".format(pattern)).replace("\\", "/")
+    fallback_path = "'" + os.path.join(icons_directory, "empty.svg'").replace("\\", "/")
+    svg_expr = "if_not_exists({svg_path}, {fallback_path})".format(svg_path=svg_path, fallback_path=fallback_path)
+    return """<!-- {description} -->
+          <symbol alpha="{opacity}" clip_to_extent="1" type="fill" name="{index}">
+        <layer pass="{rendering_pass}" class="SVGFill" locked="0">
+          <prop k="angle" v="0"/>
+          <prop k="color" v="255,255,255,255"/>
+          <prop k="outline_color" v="0,0,0,255"/>
+          <prop k="outline_width" v="0.2"/>
+          <prop k="outline_width_map_unit_scale" v="0,0,0,0,0,0"/>
+          <prop k="outline_width_unit" v="Pixel"/>
+          <prop k="pattern_width_map_unit_scale" v="0,0,0,0,0,0"/>
+          <prop k="pattern_width_unit" v="Pixel"/>
+          <prop k="svgFile" v=""/>
+          <prop k="svgFile_dd_expression" v="{svg_path}"/>
+          <prop k="svgFile_dd_active" v="1"/>
+          <prop k="svgFile_dd_field" v=""/>
+          <prop k="svgFile_dd_useexpr" v="1"/>
+          <prop k="svg_outline_width_map_unit_scale" v="0,0,0,0,0,0"/>
+          <prop k="svg_outline_width_unit" v="MM"/>
+          <prop k="width" v="17"/>
+          <symbol alpha="0" clip_to_extent="1" type="line" name="@3@0">
+            <layer pass="0" class="SimpleLine" locked="0">
+              <prop k="capstyle" v="square"/>
+              <prop k="customdash" v="5;2"/>
+              <prop k="customdash_map_unit_scale" v="0,0,0,0,0,0"/>
+              <prop k="customdash_unit" v="MM"/>
+              <prop k="draw_inside_polygon" v="0"/>
+              <prop k="joinstyle" v="bevel"/>
+              <prop k="line_color" v="0,0,0,255"/>
+              <prop k="line_style" v="solid"/>
+              <prop k="line_width" v="0"/>
+              <prop k="line_width_unit" v="MM"/>
+              <prop k="offset" v="0"/>
+              <prop k="offset_map_unit_scale" v="0,0,0,0,0,0"/>
+              <prop k="offset_unit" v="MM"/>
+              <prop k="use_custom_dash" v="0"/>
+              <prop k="width_map_unit_scale" v="0,0,0,0,0,0"/>
+            </layer>
+          </symbol>
+        </layer>
+      </symbol>""".format(description=label,
+                          opacity=opacity,
+                          index=index,
+                          svg_path=svg_expr,
+                          rendering_pass=rendering_pass)
+
+
+def _get_fill_symbol_xml(fill_color_rgba, fill_outline_color_rgba, index, label, offset, opacity, rendering_pass):
+    symbol = """<!-- {description} -->
+        <symbol alpha="{opacity}" clip_to_extent="1" type="fill" name="{index}">
+                <layer pass="{rendering_pass}" class="SimpleFill" locked="0">
+                    <prop k="border_width_map_unit_scale" v="0,0,0,0,0,0"/>
+                    <prop k="color" v="{fill_color}"/>
+                    <prop k="joinstyle" v="bevel"/>
+                    <prop k="offset" v="{offset}"/>
+                    <prop k="offset_map_unit_scale" v="0,0,0,0,0,0"/>
+                    <prop k="offset_unit" v="Pixel"/>
+                    <prop k="outline_color" v="{fill_outline_color}"/>
+                    <prop k="outline_style" v="solid"/>
+                    <prop k="outline_width" v="0.7"/>
+                    <prop k="outline_width_unit" v="Pixel"/>
+                    <prop k="style" v="solid"/>
+                </layer>
+            </symbol>
+            """.format(opacity=opacity,
+                       index=index,
+                       fill_color=fill_color_rgba,
+                       fill_outline_color=fill_outline_color_rgba,
+                       offset=offset,
+                       description=label,
+                       rendering_pass=rendering_pass)
+    return symbol
 
 
 def _get_line_symbol(index, style):
