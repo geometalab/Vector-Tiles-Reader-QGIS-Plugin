29c937abe1af8f2db3f18bc6aa38958145b258a0
samwisee <mboos@outlook.com>
2017-11-30 21:33:08 +0100

    update background color setting with reset buttons

--- ui/dialogs.py
+++ ui/dialogs.py
@@ -356,26 +356,26 @@ class OptionsGroup(QGroupBox, Ui_OptionsGroup):
 
     def _reset_to_basemap_defaults(self):
         self._set_settings(auto_zoom=True, fix_zoom=False, tile_limit=32, styles_enabled=True, merging_enabled=False,
-                           clip_tile_at_bounds=False)
+                           clip_tile_at_bounds=False, background_color=True)
         self._is_inspection_mode = False
         self.btnResetToAnalysisDefaults.setChecked(False)
         self.btnResetToInspectionDefaults.setChecked(False)
 
     def _reset_to_analysis_defaults(self):
         self._set_settings(auto_zoom=False, fix_zoom=True, tile_limit=10, styles_enabled=False, merging_enabled=True,
-                           clip_tile_at_bounds=True)
+                           clip_tile_at_bounds=True, background_color=False)
         self._is_inspection_mode = False
         self.btnResetToBasemapDefaults.setChecked(False)
         self.btnResetToInspectionDefaults.setChecked(False)
 
     def _reset_to_inspection_defaults(self):
         self._set_settings(auto_zoom=False, fix_zoom=False, tile_limit=1, styles_enabled=False, merging_enabled=False,
-                           clip_tile_at_bounds=False)
+                           clip_tile_at_bounds=False, background_color=False)
         self._is_inspection_mode = True
         self.btnResetToBasemapDefaults.setChecked(False)
         self.btnResetToAnalysisDefaults.setChecked(False)
 
-    def _set_settings(self, auto_zoom, fix_zoom, tile_limit, styles_enabled, merging_enabled, clip_tile_at_bounds):
+    def _set_settings(self, auto_zoom, fix_zoom, tile_limit, styles_enabled, merging_enabled, clip_tile_at_bounds, background_color):
         self.rbZoomMax.setChecked(not auto_zoom and not fix_zoom)
         self.rbAutoZoom.setChecked(auto_zoom)
         self.rbZoomManual.setChecked(fix_zoom)
@@ -386,6 +386,7 @@ class OptionsGroup(QGroupBox, Ui_OptionsGroup):
         self.chkApplyStyles.setChecked(styles_enabled)
         self.chkMergeTiles.setChecked(merging_enabled)
         self.chkClipTiles.setChecked(clip_tile_at_bounds)
+        self.chkSetBackgroundColor.setChecked(background_color)
 
     def set_omt_styles_enabled(self, enabled):
         self._set_option(self._APPLY_STYLES, enabled)
