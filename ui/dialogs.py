06d961382c21176ba00be142b9c585b2047475aa
samwisee <mboos@outlook.com>
2018-11-01 10:57:03 +0100

    bugfix: calls changed to connect_to from connect

--- ui/dialogs.py
+++ ui/dialogs.py
@@ -142,8 +142,8 @@ class ConnectionsDialog(QDialog, Ui_DlgConnections):
         if not connection_to_select:
             connection_to_select = self._OMT
         self.tilejson_connections.select_connection(connection_to_select)
-        self.btnConnectDirectory.clicked.connect(lambda: self.connect(self._directory_conn))
-        self.btnConnectFile.clicked.connect(lambda: self.connect(self._mbtiles_conn))
+        self.btnConnectDirectory.clicked.connect(lambda: self.connect_to(self._directory_conn))
+        self.btnConnectFile.clicked.connect(lambda: self.connect_to(self._mbtiles_conn))
         self.tabConnections.currentChanged.connect(self._handle_tab_change)
         self.tilejson_connections.on_connect.connect(self._handle_connect)
         self.tilejson_connections.on_connection_change.connect(self._handle_connection_change)
